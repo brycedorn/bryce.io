9b12f0aeae12dd23a386f51c85de0d461589445e
Bryce Dorn <brycedorn@gmail.com>
2014-05-03 12:55:19 -0500

    not sure why paper cdn wasnt working, look into later

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,54 @@
-(function(a){(jQuery.browser=jQuery.browser||{}).mobile=/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))})(navigator.userAgent||navigator.vendor||window.opera);$(document).ready(function(){$("#moon").click(function(){$(this).css("width",$(window).width()*.95);$(this).css("height",$(window).height()*.9);$("#works").css("height",$(window).height()*.7);$(this).addClass("open");$("#portfolio").css("display","block");$("#close").css("opacity","1").delay(500).fadeIn("slow")});$("#close").click(function(){revert()});$(document).on("keyup",function(e){if(e.keyCode===27&&$("#moon").hasClass("open")){revert()}});function revert(){$("#moon").removeClass("open");$("#moon").css("width","30%");$("#moon").css("height","50%");$("#portfolio").css("display","none");$("#close").css("opacity","0").fadeOut("fast")}if($(window).width()<768||jQuery.browser.mobile){$("canvas").hide();$("#moon").hide();$("#portfolio").css("display","none");$("#mobile").unhide()}else{$("#mobile").hide()}var n=(new Date).getHours();if(n>7&&n<19){$("#moon").css("background","#FFE545");$("#moon").addClass("sunny");$("body").css("background","#6cc3fc")}});var max_width=$(window).width();var max_height=$(window).height();var d=new Date;var n=d.getHours();var night=n>7&&n<19?false:true;var moveStars=new function(){if(night)var star_count=200;else var star_count=0;var star=new Path.Circle({center:[0,0],radius:2,fillColor:"EEEAF2"});var symbol=new Symbol(star);for(var i=0;i<star_count;i++){var center=Point.random()*view.size;var placed=symbol.place(center);placed.scale(i/star_count+.01);placed.data={vector:new Point({angle:Math.random()*20,length:i/star_count*Math.random()/5})}}function keepInView(item){var position=item.position;var viewBounds=view.bounds;if(position.isInside(viewBounds))return;var itemBounds=item.bounds;if(position.x>viewBounds.width+5){position.x=-item.bounds.width}if(position.x<-itemBounds.width-5){position.x=viewBounds.width}}return function(vector){var layer=project.activeLayer;for(var i=0;i<star_count;i++){var item=layer.children[i];var size=item.bounds.size;var length=vector.length/20*size.width/20;item.position+=vector.normalize(length)+item.data.vector;keepInView(item)}}};var segment_amount=5;var height=30;var sway_amount=.08;var font_size=14-(1400-max_width)/100+"em";var interval=.0025;var thickness=500;var path=new Path({strokeColor:"0C0F66",strokeWidth:thickness,strokeCap:"square"});var path2=new Path({strokeColor:"#0B108C",strokeWidth:thickness,strokeCap:"square"});var text=new PointText({content:"bryce",fontSize:font_size,fillColor:"EEEAF2",fontFamily:"Georgia"});text.position=new Point(max_width*.25,max_height*.6);var path3=new Path({strokeColor:"rgba(4,78,173,0.8)",strokeWidth:thickness,strokeCap:"square"});for(var i=0;i<=segment_amount;i++){path.add(new Point(i/segment_amount,1)*view.size);path2.add(new Point(i/segment_amount,1)*view.size);path3.add(new Point(i/segment_amount,1)*view.size)}var time=0;function onFrame(event){var j=1;var k=2;for(var i=0;i<=segment_amount;i++){var lastSinus;var lastCosinus;if(j>segment_amount)j=0;if(k>segment_amount)k=0;var segment=path.segments[i];var segment2=path2.segments[j];var segment3=path3.segments[k];var sinus=Math.sin(time*2+i);var cosinus=Math.cos(time*2+i);segment.point.y=sinus*(height-10)+700;segment2.point.y=sinus*(height-5)+780;text.position.y=sinus*(height+10)+580;if(i<segment_amount/2)text.rotate(sinus*sway_amount);else text.rotate(-cosinus*sway_amount);segment3.point.y=sinus*height+860;j++;k++;lastSinus=sinus;lastCosinus=cosinus;time+=interval}path.smooth();path2.smooth();path3.smooth();var tmp={x:max_width,y:max_height/2};var vector=(view.center-tmp)/10;moveStars(vector/2)}
+/**
+ * jQuery.browser.mobile (http://detectmobilebrowser.com/)
+ **/
+(function(a){(jQuery.browser=jQuery.browser||{}).mobile=/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))})(navigator.userAgent||navigator.vendor||window.opera);
+
+$(document).ready(function() {
+  // Moon
+  $("#moon").click(function() {
+    $(this).css('width',$(window).width()*0.95);
+    $(this).css('height',$(window).height()*0.9);
+    $("#works").css('height',$(window).height()*0.7);
+    $(this).addClass('open');
+    $("#portfolio").css('display','block');
+    $("#close").css('opacity','1').delay(500).fadeIn('slow');
+  });
+
+  // Close
+  $("#close").click(function() {
+    revert();
+  });
+
+  // Esc
+  $(document).on('keyup', function(e) {
+    if(e.keyCode === 27 && $("#moon").hasClass('open')) {
+      revert();
+    }
+  });
+
+  function revert() {
+    $("#moon").removeClass('open');
+    $("#moon").css('width','30%');
+    $("#moon").css('height','50%');
+    $("#portfolio").css('display','none');
+    $("#close").css('opacity','0').fadeOut('fast');
+  }
+
+  // Mobile
+  if($(window).width() < 768 || jQuery.browser.mobile) {
+    $('canvas').hide();
+    $('#moon').hide();
+    $("#portfolio").css('display','none');
+    $('#mobile').unhide();
+  } else { // Desktop
+    $('#mobile').hide();
+  }
+
+  // Daytime
+  var n = new Date().getHours();
+  if(n>7 && n <19) {
+    $('#moon').css('background','#FFE545');
+    $('#moon').addClass('sunny');
+    $('body').css('background','#6cc3fc');
+  }
+});
