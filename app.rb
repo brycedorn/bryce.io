61be4944ae6d19a633b6dc1f0c55c85b08473f2a
Bryce Dorn <brycedorn@gmail.com>
2014-05-04 22:14:09 -0500

    no sense in cluttering

--- app.rb
+++ app.rb
@@ -6,6 +6,10 @@ class App < Sinatra::Base
   get '/' do
     haml :index
   end
+
+  get '/lineup' do
+    haml :lineup
+  end
   
   # Start the server if ruby file executed directly
   run! if app_file == $0
