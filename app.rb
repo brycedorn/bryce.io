83c5e3c16825f0ce11d85e7e2401010cc942041a
Bryce Dorn <brycedorn@gmail.com>
2014-05-25 17:01:09 -0500

    adds route to redirect minitranslate

--- app.rb
+++ app.rb
@@ -6,6 +6,10 @@ class App < Sinatra::Base
   get '/' do
     haml :index
   end
+
+  get '/minitranslate' do
+    redirect to('http://brycedorn.github.io/minitranslate')
+  end
   
   # Start the server if ruby file executed directly
   run! if app_file == $0
